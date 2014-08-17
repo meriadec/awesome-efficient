65b2bc930efe9e379e21c19ec467f0a73ba3d109
meriadec <meriadec.pillet@gmail.com>
2014-08-18 01:32:53 +0200

    float + foreground

--- rc.lua
+++ rc.lua
@@ -538,7 +538,12 @@ globalkeys = awful.util.table.join(
 clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
     awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end),
-    awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
+
+    awful.key({ modkey, "Control" }, "space",  function (c)
+      awful.client.floating.toggle()
+      c.ontop = not c.ontop
+    end),
+
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
     awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
     awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
