1a8cd88fe283cb9a1276cfa11ae847c4b8bec652
meriadec <meriadec.pillet@gmail.com>
2014-12-03 19:42:07 +0100

    add separator

--- rc.lua
+++ rc.lua
@@ -279,6 +279,7 @@ for s = 1, screen.count() do
     local left_layout = wibox.layout.fixed.horizontal()
     --left_layout:add(spr)
     left_layout:add(mytaglist[s])
+    left_layout:add(spr)
     left_layout:add(mypromptbox[s])
     left_layout:add(spr)
 
