4eb51ab22b288db77fb7d27c5aed2746afb8d58f
meriadec <meriadec.pillet@gmail.com>
2017-03-21 19:59:23 +0100

    reformat

--- rc.lua
+++ rc.lua
@@ -485,20 +485,41 @@ globalkeys = awful.util.table.join(
 )
 
 clientkeys = awful.util.table.join(
-    awful.key({ modkey,           }, "f",
-        function (c)
-            c.fullscreen = not c.fullscreen
-            c:raise()
-        end,
-        {description = "toggle fullscreen", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
-              {description = "close", group = "client"}),
-    awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
-              {description = "toggle floating", group = "client"}),
-    awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
-              {description = "move to master", group = "client"}),
-    awful.key({ modkey,           }, "p",      function (c) c.ontop = not c.ontop            end,
-              {description = "toggle keep on top", group = "client"})
+
+  awful.key({ modkey }, "f",
+    function (c)
+      c.fullscreen = not c.fullscreen
+      c:raise()
+    end,
+    { description = "toggle fullscreen", group = "client" }
+  ),
+
+  awful.key({ modkey, "Shift" }, "c",
+    function (c)
+      c:kill()
+    end,
+    {description = "close", group = "client"}
+  ),
+
+  awful.key({ modkey, "Control" }, "space",
+    awful.client.floating.toggle,
+    { description = "toggle floating", group = "client" }
+  ),
+
+  awful.key({ modkey, "Control" }, "Return",
+    function (c)
+      c:swap(awful.client.getmaster())
+    end,
+    { description = "move to master", group = "client" }
+  ),
+
+  awful.key({ modkey }, "p",
+    function (c)
+      c.ontop = not c.ontop
+    end,
+    { description = "toggle keep on top", group = "client" }
+  )
+
 )
 
 -- Bind all key numbers to tags.
