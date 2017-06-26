ff2b8faab766a5fa8f66a3aed134e92fe8cd4d3e
meriadec <meriadec.pillet@gmail.com>
2017-06-27 00:12:44 +0200

    hide bar at start

--- rc.lua
+++ rc.lua
@@ -331,7 +331,7 @@ awful.screen.connect_for_each_screen(function(s)
     s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist_buttons)
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s, height = 30 })
+    s.mywibox = awful.wibar({ position = "top", screen = s, height = 30, visible = false })
 
     -- Add widgets to the wibox
     s.mywibox:setup {
